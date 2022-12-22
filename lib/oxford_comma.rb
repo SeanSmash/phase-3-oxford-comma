0e7026b4bcdb8c8afc92da53ea57ebf8f90d2133
Sean Sarokas <sps4462@gmail.com>
2022-12-22 09:12:47 -0500

    commit tree 5b1a6528da743f46cf78e604c445951d304ca4fe

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+    if array.length == 1
+        array[0]
+    elsif array.length == 2
+        string = array[0] + " and " + array[1]
+    elsif array.length > 2
+        #binding.pry
+        word = array.pop
+        string = array.join(", ")
+        string << ", and #{word}" 
+        #binding.pry
+    end
+    
 end
